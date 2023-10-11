0eef30b41afa0a55f98ddb2989cbe48e85a4cb0d
Valéria <vvqf2016@gmail.com>
2023-10-11 18:20:17 -0300

    added models 'Artistas' and 'SeloFonografico'

--- Atividade5/core/admin.py
+++ Atividade5/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Discos
+from .models import Discos, SeloFonografico, Artista
 
 # Register your models here.
 admin.site.register(Discos)
+admin.site.register(SeloFonografico)
+admin.site.register(Artista)
--- Atividade5/core/models.py
+++ Atividade5/core/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+class SeloFonografico(models.Model):
+    nome = models.CharField(max_length = 200)
+    def __str__(self):
+        return (f'selo fonográfico: {self.nome}')
+class Artista(models.Model):
+    nome = models.CharField(max_length=200)
+    def __str__(self):
+        return (f'Nome: {self.nome}')
+
 class Discos(models.Model):
     nome = models.CharField(max_length = 200)
     descricao = models.TextField()
-    selo_fonografico = models.CharField(max_length=200)
     pais = models.CharField(max_length = 200)
     ano = models.IntegerField()
     genero = models.CharField(max_length = 200)
     quantidade = models.IntegerField()
-    
+    selo_fonografico = models.ForeignKey(SeloFonografico, on_delete = models.CASCADE)
+    artistas = models.ManyToManyField(Artista)
+
+    def __str__(self):
+        todos_artistas = ', '.join([artistas.nome for artistas in self.artistas.all()])
+        return (f'Nome: {self.nome}, Artistas: {todos_artistas}, Gênero: {self.genero} selo fonográfico: {self.selo_fonografico}')
